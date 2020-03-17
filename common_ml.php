6af0a6e75e5e47989c1bcb343209f4bd18fc93a5
Roman Romanov <nicodim@mail.ru>
2020-03-17 15:27:56 +0300

    Delete common_ml.php

--- common_ml.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!class_exists("\\SitemapXML\\SitemapXML")) {
-    include_once(__DIR__ . "/classes/SitemapXML.php");
-}
-if (!class_exists("\\SitemapXML\\SitemapRuntimeTable")) {
-    include_once(__DIR__ . "/classes/SitemapRuntimeTable.php");
-}
-
-?>
